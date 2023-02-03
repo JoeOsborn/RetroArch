d340ba0c61b663a7a9ee87ec69a359a9fe517eab
Joseph C. Osborn <jcoa2018@pomona.edu>
2023-02-03 11:44:07 -0800

    remove dependencies on jquery and others for embed player
    Also notice when savestates happen

--- pkg/emscripten/libretro/libretro_embed.js
+++ pkg/emscripten/libretro/libretro_embed.js
@@ -9,6 +9,8 @@ const content = "bfight.nes";
 const entryState = false;
 const movie = false;
 
+const FS_CHECK_INTERVAL = 1000;
+
 console.assert(!(entryState && movie), "It is invalid to have both an entry state and play back a movie");
 
 var movieArg = "-R";
@@ -27,7 +29,7 @@ if (movie) {
     retro_args.push("/home/web_user/content/movie.bsv");
 } else {
     retro_args.push("-R");
-    retro_args.push("/home/web_user/userdata/movie.bsv");
+    retro_args.push("/home/web_user/retroarch/userdata/movie.bsv");
 }
 retro_args.push("/home/web_user/content/"+content);
 
@@ -37,49 +39,69 @@ var initializationCount = 0;
 
 function cleanupStorage()
 {
-   localStorage.clear();
-   document.getElementById("btnClean").disabled = true;
+    localStorage.clear();
+    document.getElementById("btnClean").disabled = true;
 }
 
 function fsInit()
 {
-   $('#icnLocal').removeClass('fa-globe');
-   $('#icnLocal').addClass('fa-spinner fa-spin');
-   afs = new BrowserFS.FileSystem.InMemory();
-   setupFileSystem("browser");
-   appInitialized();
+    afs = new BrowserFS.FileSystem.InMemory();
+    setupFileSystem("browser");
+    appInitialized();
 }
 
 function appInitialized()
 {
-     /* Need to wait for both the file system and the wasm runtime 
-        to complete before enabling the Run button. */
-     initializationCount++;
-     if (initializationCount == 2)
-     {
-         preLoadingComplete();
-     }
- }
+    /* Need to wait for both the file system and the wasm runtime 
+       to complete before enabling the Run button. */
+    initializationCount++;
+    if (initializationCount == 2)
+    {
+        preLoadingComplete();
+    }
+}
 
 function preLoadingComplete()
 {
-   /* Make the Preview image clickable to start RetroArch. */
-   $('.webplayer-preview').addClass('loaded').click(function () {
-      startRetroArch();
-      return false;
-  });
+    /* Make the Preview image clickable to start RetroArch. */
+    document.getElementById("webplayer-preview").classList.add("loaded");
+    document.getElementById("webplayer-preview").addEventListener(
+        "click",
+        function () {
+            startRetroArch();
+            return false;
+        });
+}
+
+var renderedSaves = [];
+function checkChangedSaves() {
+    try {
+        var newSaves = FS.readdir("/home/web_user/retroarch/userdata/states");
+        // if any new ones, update lastSaves
+        for(var i = renderedSaves.length; i < newSaves.length; i++) {
+            let save = newSaves[i];
+            console.log(save);
+            renderedSaves.push(save);
+        }
+    } catch(e) {
+        if (e instanceof FS.ErrnoError) {
+            // do nothing
+        } else {
+            throw e;
+        }
+    }
 }
 
 function setupFileSystem(backend)
 {
-   /* create a mountable filesystem that will server as a root
-      mountpoint for browserfs */
-   var mfs =  new BrowserFS.FileSystem.MountableFileSystem();
+    /* create a mountable filesystem that will server as a root
+       mountpoint for browserfs */
+    var mfs =  new BrowserFS.FileSystem.MountableFileSystem();
 
-   /* create an XmlHttpRequest filesystem for the bundled data */
-   var xfs1 =  new BrowserFS.FileSystem.XmlHttpRequest
-      (".index-xhr", "assets/frontend/bundle/");
-   /* create an XmlHttpRequest filesystem for core assets */
+    /* create an XmlHttpRequest filesystem for the bundled data */
+    var xfs1 =  new BrowserFS.FileSystem.XmlHttpRequest
+    (".index-xhr", "assets/frontend/bundle/");
+    /* create an XmlHttpRequest filesystem for core assets */
     // var xfs2 =  new BrowserFS.FileSystem.XmlHttpRequest
     // ([], "assets/cores/");
     var xfs_content_files = {"retroarch.cfg":null};
@@ -94,6 +116,7 @@ function setupFileSystem(backend)
 
     console.log("WEBPLAYER: initializing filesystem: " + backend);
     mfs.mount('/home/web_user/retroarch/userdata', afs);
+    setInterval(checkChangedSaves, FS_CHECK_INTERVAL);
 
     mfs.mount('/home/web_user/retroarch/bundle', xfs1);
     // mfs.mount('/home/web_user/retroarch/userdata/content/downloads', xfs2);
@@ -118,152 +141,110 @@ function copyFile(from, to) {
 
 function startRetroArch()
 {
-   $('.webplayer').show();
-   $('.webplayer-preview').hide();
+    document.getElementById("canvas").classList.remove("hidden");
+    document.getElementById("webplayer-preview").classList.add("hidden");
 
-   $('#btnMenu').removeClass('disabled');
-   $('#btnAdd').removeClass('disabled');
-   $('#btnRom').removeClass('disabled');
-
-   document.getElementById("btnAdd").disabled = false;
-   document.getElementById("btnRom").disabled = false;
-   document.getElementById("btnMenu").disabled = false;
-
-   Module['callMain'](Module['arguments']);
-   Module['resumeMainLoop']();
-   document.getElementById('canvas').focus();
-}
-
-function selectFiles(files)
-{
-   $('#btnAdd').addClass('disabled');
-   $('#icnAdd').removeClass('fa-plus');
-   $('#icnAdd').addClass('fa-spinner spinning');
-   var count = files.length;
-
-   for (var i = 0; i < count; i++)
-   {
-      filereader = new FileReader();
-      filereader.file_name = files[i].name;
-      filereader.readAsArrayBuffer(files[i]);
-      filereader.onload = function(){uploadData(this.result, this.file_name);};
-      filereader.onloadend = function(evt)
-      {
-         console.log("WEBPLAYER: file: " + this.file_name + " upload complete");
-         if (evt.target.readyState == FileReader.DONE)
-         {
-            $('#btnAdd').removeClass('disabled');
-            $('#icnAdd').removeClass('fa-spinner spinning');
-            $('#icnAdd').addClass('fa-plus');
-         }
-      };
-   }
-}
-
-function uploadData(data,name)
-{
-   var dataView = new Uint8Array(data);
-   FS.createDataFile('/', name, dataView, true, false);
-
-   var data = FS.readFile(name,{ encoding: 'binary' });
-   FS.writeFile('/home/web_user/retroarch/userdata/content/' + name, data ,{ encoding: 'binary' });
-   FS.unlink(name);
+    Module['callMain'](Module['arguments']);
+    Module['resumeMainLoop']();
+    document.getElementById('canvas').focus();
 }
 
 var Module =
-{
-  noInitialRun: true,
-    arguments: retro_args,
-  preRun: [],
-  postRun: [],
-  onRuntimeInitialized: function()
-  {
-     appInitialized();
-  },
-  print: function(text)
-  {
-     console.log(text);
-  },
-  printErr: function(text)
-  {
-     console.log(text);
-  },
-  canvas: document.getElementById('canvas'),
-  totalDependencies: 0,
-  monitorRunDependencies: function(left)
-  {
-     this.totalDependencies = Math.max(this.totalDependencies, left);
-  }
-};
+    {
+        noInitialRun: true,
+        arguments: retro_args,
+        preRun: [],
+        postRun: [],
+        onRuntimeInitialized: function()
+        {
+            appInitialized();
+        },
+        print: function(text)
+        {
+            console.log(text);
+        },
+        printErr: function(text)
+        {
+            console.log(text);
+        },
+        canvas: document.getElementById('canvas'),
+        totalDependencies: 0,
+        monitorRunDependencies: function(left)
+        {
+            this.totalDependencies = Math.max(this.totalDependencies, left);
+        }
+    };
+
+function domLoaded() {
+    /**
+     * Attempt to disable some default browser keys.
+     */
+	var keys = {
+        9: "tab",
+        13: "enter",
+        16: "shift",
+        18: "alt",
+        27: "esc",
+        33: "rePag",
+        34: "avPag",
+        35: "end",
+        36: "home",
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down",
+        112: "F1",
+        113: "F2",
+        114: "F3",
+        115: "F4",
+        116: "F5",
+        117: "F6",
+        118: "F7",
+        119: "F8",
+        120: "F9",
+        121: "F10",
+        122: "F11",
+        123: "F12"
+    };
+    window.addEventListener('keydown', function (e) {
+        if (keys[e.which]) {
+            e.preventDefault();
+        }
+    });
+
+    if (!core) {
+        core = 'gambatte';
+    }
+    
+    // Load the Core's related JavaScript.
+    var coreScript = document.createElement('script');
+    coreScript.type = 'text/javascript';
+    coreScript.src = core+'_libretro.js';
+    coreScript.addEventListener("error", function() {console.error("Couldn't load core file", core+"_libretro.js");});
+    coreScript.addEventListener("load", function() {
+                fsInit();
+        });
+    
+    document.head.appendChild(coreScript);
+}
 
 // When the browser has loaded everything.
-$(function() {
-   // Enable all available ToolTips.
-   $('.tooltip-enable').tooltip({
-      placement: 'right'
-   });
+if (document.readyState == 'loading') {
+    document.addEventListener("DOMContentLoaded", domLoaded);
+} else {
+    domLoaded();
+}
 
-   /**
-    * Attempt to disable some default browser keys.
-    */
-	var keys = {
-    9: "tab",
-    13: "enter",
-    16: "shift",
-    18: "alt",
-    27: "esc",
-    33: "rePag",
-    34: "avPag",
-    35: "end",
-    36: "home",
-    37: "left",
-    38: "up",
-    39: "right",
-    40: "down",
-    112: "F1",
-    113: "F2",
-    114: "F3",
-    115: "F4",
-    116: "F5",
-    117: "F6",
-    118: "F7",
-    119: "F8",
-    120: "F9",
-    121: "F10",
-    122: "F11",
-    123: "F12"
-  };
-	window.addEventListener('keydown', function (e) {
-    if (keys[e.which]) {
-      e.preventDefault();
-    }
-  });
-
-   if (!core) {
-      core = 'gambatte';
-   }
-   // Load the Core's related JavaScript.
-   $.getScript(core + '_libretro.js', function ()
-   {
-      fsInit();
-   });
- });
-
+/* Simulate a key event */
 function keyPress(k)
 {
-   kp(k, "keydown");
-   setTimeout(function(){kp(k, "keyup")}, 50);
+    kp(k, "keydown");
+    setTimeout(function(){kp(k, "keyup");}, 50);
 }
 
 kp = function(k, event) {
-   var oEvent = new KeyboardEvent(event, { code: k });
+    var oEvent = new KeyboardEvent(event, { code: k });
 
-   document.dispatchEvent(oEvent);
-   document.getElementById('canvas').focus();
-}
-
-
-function downloadFiles(path) {
-    const root = "/home/web_user/retroarch/userdata";
-    
+    document.dispatchEvent(oEvent);
+    document.getElementById('canvas').focus();
 }
